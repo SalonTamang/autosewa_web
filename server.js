867c450f1f7e72c1f1ab51ad5e39be74787658a4
barungurung00@gmail.com <barun>
2023-12-25 01:40:39 +0000

    added footer section

--- server.js
+++ server.js
@@ -39,4 +39,70 @@ app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
 
+const dummyData = [
+    {
+      "id": 1,
+      "name": "John Doe",
+      "age": 28,
+      "phoneNumber": "1234567890"
+    },
+    {
+      "id": 2,
+      "name": "Jane Smith",
+      "age": 32,
+      "phoneNumber": "9876543210"
+    },
+    {
+      "id": 3,
+      "name": "Michael Johnson",
+      "age": 45,
+      "phoneNumber": "5555555555"
+    },
+    // Add more objects here with different values
+    {
+      "id": 4,
+      "name": "Emily Davis",
+      "age": 22,
+      "phoneNumber": "4444444444"
+    },
+    {
+      "id": 5,
+      "name": "Robert Wilson",
+      "age": 37,
+      "phoneNumber": "7777777777"
+    },
+    {
+      "id": 6,
+      "name": "Sophia Brown",
+      "age": 19,
+      "phoneNumber": "6666666666"
+    },
+    {
+      "id": 7,
+      "name": "William Taylor",
+      "age": 50,
+      "phoneNumber": "2222222222"
+    },
+    {
+      "id": 8,
+      "name": "Olivia Anderson",
+      "age": 27,
+      "phoneNumber": "8888888888"
+    },
+    {
+      "id": 9,
+      "name": "James Martinez",
+      "age": 33,
+      "phoneNumber": "9999999999"
+    },
+    {
+      "id": 10,
+      "name": "Ava Clark",
+      "age": 41,
+      "phoneNumber": "3333333333"
+    }
+]
 // Path: public/login.html
+app.get("/getData", (req,res)=>{
+    res.json(dummyData)
+})
