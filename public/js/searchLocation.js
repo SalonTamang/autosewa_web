556416f63f307b096aa7da4c14b0a0f7dd38ece3
salon <salon.ghalan@gmail.com>
2023-12-27 05:01:33 +0000

    sortign database connection and adding faker

---
+++ public/js/searchLocation.js
@@ -0,0 +1,44 @@
+
+//Set up page when window has loaded
+window.onload = searchRiders();
+
+let mainContainer = document.querySelector(".container");
+
+function searchRiders(){
+    // Get the select element
+    var selectElement = document.querySelector('.search-select');
+
+    // Get the value of the selected option
+    var station = selectElement.value;
+
+    let drivers = document.querySelector(".driverList");
+
+    //clear the files
+    drivers.innerHTML = "";
+    let xml = new XMLHttpRequest();
+    xml.onload = () =>{
+        let response = JSON.parse(xml.responseText);
+        // console.log(response);
+        if("error" in response){
+            mainContainer.innerHTML = response.error;
+        } else {     
+            //if we get result from database
+            for (let index = 0; index < response.length; index++) {
+                let driverID = response[index]['id'];
+                let firstName = response[index]['firstName'];
+                let lastName = response[index]['lastName'];
+                let image = response[index]['image'];               
+
+                drivers.innerHTML += "<div class='driver'><div class='driver-image'><img src='images/" + image + ".png' alt='dimg' /></div><div class='driver-info'><h3>" + firstName + " " + lastName + " </h3><p onclick='getDInfo(" + driverID + ")'>View info</p></div></div>";
+            }
+
+        }
+    }
+    var data = {};
+    data.station = station;
+    xml.open("GET", "/getDrivers");
+    xml.setRequestHeader("Content-type", "application/json");
+    xml.send(JSON.stringify(data));
+}
+
+
--- server.js
+++ server.js
@@ -2,23 +2,16 @@ const express = require('express');
 const mysql = require('mysql');
 var morgan = require("morgan");
 var path = require("path");
+const faker = require("faker");
 
 // Create a connection to the database
-// const db = mysql.createConnection({
-//     host: 'localhost',
-//     user: 'your_username',
-//     password: 'your_password',
-//     database: 'your_database'
-// });
-
-// Connect to the database
-// db.connect((err) => {
-//     if (err) {
-//         console.error('Error connecting to the database:', err);
-//         return;
-//     }
-//     console.log('Connected to the database');
-// });
+const connection = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'meroauto',
+    port: 3306
+});
 
 // Create an Express application
 const app = express();
@@ -33,77 +26,80 @@ app.get("/", function(req, res){
   res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
+// response with limited number of rider, Pagination
+// search the driver in the particular station
+app.get("/getDrivers", (req,res)=>{
+  var sql;
+  var limit = 15;
+  var pageNumber = req.query.page || 1; //page send from the url eg. localhost:3000/getDrivers?station=buddasubba&pageNumber=1
+  var offset = (pageNumber-1) * limit;
+  var station = req.query.station || "all";
+  station = station.toLowerCase();
+
+  if(station == "all"){
+    sql = `SELECT * FROM riders 
+    LIMIT ${limit} OFFSET ${offset} `;
+  }else{
+    sql = "SELECT * FROM riders WHERE station = '" + station + "' "+
+    ` LIMIT ${limit} OFFSET ${offset}`;
+  }
+  connection.query(sql, (err, result) =>{
+      if(err){
+          return res.status(500).send('{"message": false, "error": "' +
+          JSON.stringify(err) + '"}');
+      } else{
+          res.send(result);
+      }
+  });
+});
+
 // Start the server
 const port = 3000;
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
 
-const dummyData = [
-    {
-      "id": 1,
-      "name": "John Doe",
-      "age": 28,
-      "phoneNumber": "1234567890"
-    },
-    {
-      "id": 2,
-      "name": "Jane Smith",
-      "age": 32,
-      "phoneNumber": "9876543210"
-    },
-    {
-      "id": 3,
-      "name": "Michael Johnson",
-      "age": 45,
-      "phoneNumber": "5555555555"
-    },
-    // Add more objects here with different values
-    {
-      "id": 4,
-      "name": "Emily Davis",
-      "age": 22,
-      "phoneNumber": "4444444444"
-    },
-    {
-      "id": 5,
-      "name": "Robert Wilson",
-      "age": 37,
-      "phoneNumber": "7777777777"
-    },
-    {
-      "id": 6,
-      "name": "Sophia Brown",
-      "age": 19,
-      "phoneNumber": "6666666666"
-    },
-    {
-      "id": 7,
-      "name": "William Taylor",
-      "age": 50,
-      "phoneNumber": "2222222222"
-    },
-    {
-      "id": 8,
-      "name": "Olivia Anderson",
-      "age": 27,
-      "phoneNumber": "8888888888"
-    },
-    {
-      "id": 9,
-      "name": "James Martinez",
-      "age": 33,
-      "phoneNumber": "9999999999"
-    },
-    {
-      "id": 10,
-      "name": "Ava Clark",
-      "age": 41,
-      "phoneNumber": "3333333333"
-    }
-]
 
-// Path: public/login.html
-app.get("/getData", (req,res)=>{
-    res.send(dummyData)
-})
+//register drivers based on their data
+app.post("/registerRider", (req,res)=> {
+  
+  // let firstName = req.query.firstName;
+  // let lastName = req.query.lastName;
+  // let phoneNumber = req.query.phoneNumber;
+  // let password = req.query.password;
+  // let autoNumber = req.query.autoNumber;
+  // let image = req.query.image;
+  // let station = req.query.station;
+
+
+  function getRandomStation() {
+    const stations = ["padam pokhari", "chauda", "buddha chowk", "kamane", "karra"];
+    const randomIndex = Math.floor(Math.random() * stations.length);
+    return stations[randomIndex];
+  }
+  
+  // Generate and insert 100 unique records
+  for (let i = 0; i < 100; i++) {
+    const firstName = faker.name.firstName();
+    const lastName = faker.name.lastName();
+    const phoneNumber = faker.phone.phoneNumber();
+    const password = faker.internet.password();
+    const autoNumber = faker.vehicle.vin();
+    const image = faker.image.avatar();
+    const station = getRandomStation();
+  
+    const sql = "INSERT INTO riders (firstName, lastName, phoneNumber, password, autoNumber, image, station) VALUES (?, ?, ?, ?, ?, ?, ?)";
+    const values = [firstName, lastName, phoneNumber, password, autoNumber, image, station];
+  
+    connection.query(sql, values, (err, result) => {
+      if (err) {
+        console.error('Error inserting data:', err);
+      } else {
+        console.log('Data inserted successfully:', result);
+      }
+    });
+  }
+
+});
+
+
